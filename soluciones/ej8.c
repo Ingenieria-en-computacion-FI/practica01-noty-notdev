1f46d1fa3b3bd3a5cf636339e329de8865955737
noty-notdev <rodrigo.alfredo.11001011@gmail.com>
2026-02-21 17:32:34 -0600

    + Ejercicios Calificados && Correccion de test.

--- soluciones/ej8.c
+++ soluciones/ej8.c
@@ -1,6 +1,16 @@
-
 #include <stdio.h>
+
+
 int main(){
-    // Resolver ejercicio 8
-    return 0;
+	// Resolver ejercicio 8
+
+	int m, m1_2;
+	scanf("%d %d", &m, &m1_2);
+
+	int n_col	= (m1_2 - m) / 4 - 2;
+	int total_size	= 8 * (n_col + 2);
+
+	printf("%d\n%d\n", n_col, total_size);
+
+	return 0;
 }
