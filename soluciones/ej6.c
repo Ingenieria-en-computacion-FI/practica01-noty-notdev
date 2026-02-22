d34018f20e1114de695c6d5e6c740ffd390ee576
noty-notdev <rodrigo.alfredo.11001011@gmail.com>
2026-02-21 18:19:19 -0600

    + Ejercicios comentados.

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,21 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 6
-    return 0;
+	// Resolver ejercicio 6
+
+	/*
+	 *	Sea la base 1000, las direcciones de memoria del
+	 *	arreglo "m" son:
+	 *	1000, 1004, 1008, 1012,
+	 *	1016, 1020, 1024, 1028
+	 *
+	 *	El almacenamiento es por columnas,
+	 *
+	 * 	Sea el arreglo:
+	 * 	int m[F][C];
+	 * 	entonces:
+	 *	&m[i][j] = m + (i * C + j) * 4
+	*/
+
+	return 0;
 }
--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -1,6 +1,21 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 9
-    return 0;
+	// Resolver ejercicio 9
+
+	/*
+	 *	&a[-1] y &a[5] compila porque el preprocesador las convierte en
+	 *	la expresiónes (a - 1) y (a '5), respectivamente,
+	 *	lo que no rompe la aritmética de punteros, ni la de
+	 *	arreglos.
+	 *
+	 *	Es peligroso porque no se tiene nada asignado en la dirección "a - 1",
+	 *	y manejar memoria desconocida puede corromper otros datos del sistema
+	 *	o modificar otros importantes para el funcionamiento de x o y
+	 *	programa (si es windows, en sistemas UNIX está protegido).
+	 *
+	 *	????
+	*/
+
+	return 0;
 }
